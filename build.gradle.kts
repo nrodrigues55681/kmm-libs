01b8773c484ab7081c0978a890c762e812a4ca73
Nuno Rodrigues <nuno.rodrigues@jci.com>
2021-03-22 15:10:16 +0000

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    kotlin("multiplatform") version "1.4.31"
+    id("com.android.library")
+    id("kotlin-android-extensions")
+    `maven-publish`
+}
+
+group = "pt.nrodrigues.libs"
+version = "1.0.0"
+
+repositories {
+    mavenLocal()
+    google()
+    jcenter()
+    mavenCentral()
+}
+
+kotlin {
+    android {
+        publishLibraryVariants("release", "debug")
+    }
+    iosX64("ios") {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("com.google.android.material:material:1.2.1")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation("junit:junit:4.13")
+            }
+        }
+        val iosMain by getting
+        val iosTest by getting
+    }
+}
+
+android {
+    compileSdkVersion(29)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdkVersion(24)
+        targetSdkVersion(29)
+    }
+}
